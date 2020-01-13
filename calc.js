c8537cef5fffae48aa01af34ef86042e3691531e
aadarshvikram00 <59819943+aadarshvikram00@users.noreply.github.com>
2020-01-13 14:28:04 +0530

    add the multiplication function

--- calc.js
+++ calc.js
@@ -5,10 +5,14 @@ function sum(num1,num2){
 function div(num1,num2){
     return num1/num2;
 }
+function mul(num1,num2){
+    return num1*num2;
+}
 
 function cal(){
     console.log(sum(10,20));
     console.log(div(50,25));
+    console.log(mul(3,4));
 }
 
 
